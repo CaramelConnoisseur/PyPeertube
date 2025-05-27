d2babe4050d71877ce6ff6572aee7390014bb57e
CaramelConnoisseur <210252249+CaramelConnoisseur@users.noreply.github.com>
2025-05-27 15:40:24 +0100

    first commit

---
+++ src/pypeertube/validators.py
@@ -0,0 +1,14 @@
+from re import match
+
+
+def channel_name(value: str) -> bool:
+    """Validate a Peertube channel name.
+
+    Args:
+        value (str): The proposed channel name.
+
+    Returns:
+        bool: Whether the channel name is valid.
+    """
+
+    return bool(match(r"^[0-9a-z_.]{1,50}$", value))
